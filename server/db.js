21937cbac883ae04713d807670c7dd118dd8c49c
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-12-30 20:50:58 +0530

    security improved

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://tanishq1807:tigertk1234@cluster0.cavyrzb.mongodb.net/?appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("DB connection failed", err);
