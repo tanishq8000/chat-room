43dd8b255b1998f6310be9ac7994570a33fed48a
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-12-31 19:32:06 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://chat-room-alpha-umber.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
