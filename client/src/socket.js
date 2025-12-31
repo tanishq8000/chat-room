651190a63a117d73e3cf47090fca5cc3c199e22a
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-12-31 19:28:07 +0530

    client socket updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://chat-backend.onrender.com", {
   autoConnect: false,
+  transports: ["websocket"],
 });
 
 export default socket;
