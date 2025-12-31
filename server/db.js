c476d6b1c7e5c0db33119a73a66b84a39b060a52
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-12-31 19:04:13 +0530

    env file issue resolved

--- server/db.js
+++ server/db.js
@@ -1,9 +1,7 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
